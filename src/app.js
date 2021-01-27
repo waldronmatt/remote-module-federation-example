93a4da2106ef48fc322b57f334129c0e5d256955
waldronmatt <mattwaldron@ymail.com>
2021-01-27 10:32:40 -0500

    init commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Heading from "./heading/heading.js";
+
+const heading = new Heading();
+heading.render("REMOTE (Module Federation)");
+
+import initForm from "./form/init-form.js";
+
+initForm();
