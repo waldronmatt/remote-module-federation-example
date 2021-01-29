0ac70cad38f7ffc6a58145800fa2e39184329ba5
waldronmatt <mattwaldron@ymail.com>
2021-01-28 23:07:50 -0500

    move import to the top

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import Heading from "./heading/heading.js";
+import initContactForm from "./form/init-contact-form.js";
 
 const heading = new Heading();
 heading.render("REMOTE (Module Federation)");
-
-import initContactForm from "./form/init-contact-form.js";
 
 initContactForm();
