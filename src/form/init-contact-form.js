72703a0a8c27ef8405c1e23427c98abfd1ce5f10
waldronmatt <mattwaldron@ymail.com>
2021-01-28 21:36:25 -0500

    update form with jquery mask functionality

--- src/form/init-contact-form.js
+++ src/form/init-contact-form.js
@@ -1,6 +1,7 @@
 // code from https://parsleyjs.org/doc/examples/simple.html
 import "./form-style.scss";
 import "parsleyjs";
+import "jquery-mask-plugin";
 import renderTemplate from "./template";
 import formHTML from "./form-html.tpl.html";
 import jQueryTest from "../jquery-test/jquery-test";
@@ -25,6 +26,11 @@ const setupLogic = () => {
     .on("form:submit", function () {
       return false; // Don't submit form for this demo
     });
+
+  // setup masking
+  $("[data-jquery-mask-phone]").mask("(000) 000-0000", {
+    placeholder: "__/__/____",
+  });
 };
 
 const initContactForm = () => {
